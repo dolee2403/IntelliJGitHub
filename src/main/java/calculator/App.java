d61322272f610861bb8c2803aadf1133c4ce37a2
Robbie <robbie331@>
2024-04-30 14:31:16 +0900

    Level1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.print("첫 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num1 = sc.nextInt();
