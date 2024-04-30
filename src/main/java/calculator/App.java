a74c33191fa2cee7696cf07c3b74ea7875a5fdac
Robbie <robbie331@>
2024-04-30 14:37:33 +0900

    Level1 - 1

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -10,6 +10,6 @@ public class App {
         int num1 = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
     }
 }
