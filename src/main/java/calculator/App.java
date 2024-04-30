61337ed21362f6c89e2a911a873b0e567fe1cd6c
Robbie <robbie331@>
2024-04-30 16:06:14 +0900

    Level1 - 3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -17,5 +17,23 @@ public class App {
         // charAt(매개변수:idx) : charAt 메서드는 매개변수로 char 타입으로 반환 하고자하는 문자열의 위치(index)를 받는다.
         char operator = sc.next().charAt(0);
         System.out.println("operator = " + operator);
+
+        int result = 0;
+        /* 제어문을 활용하여 위 요구사항을 만족할 수 있게 구현합니다.*/
+        if (num2 > 0) {
+            if(operator == '+') {
+                result = num1 + num2;
+            } else if (operator == '-') {
+                result = num1 - num2;
+            } else if (operator == '*') {
+                result = num1 * num2;
+            } else if (operator == '/') {
+                result = num1 / num2;
+            } else {
+                result = num1 % num2;
+            }
+        }
+
+        System.out.println("결과: " + result);
     }
 }
